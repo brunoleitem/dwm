5e2387cea8baea9bf6c66a8d9687acdbf7b94307
brunoleitem <brunoleiteem@gmail.com>
2021-07-09 20:31:10 -0300

    color fix

--- config.h
+++ config.h
@@ -1,31 +1,69 @@
 /* See LICENSE file for copyright and license details. */
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx	    = 8;
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 10;       /* snap pixel */
+static const unsigned int gappx	    = 4;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar	    = 6;
 static const int vertpadbar	    = 8;
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "Gohu GohuFont:size=10" };
+static const char dmenufont[]       = "Gohu GohuFont:size=10";
+/*static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char laranja_git[]     = "#bd93f9";
-static const char cinza_e[]	    = "#282a36";
+static const char laranja_git[]     = "#415c6d";
+static const char cinza_e[]	    = "#2A2F33";
 static const char cinza_c[]         = "#44475a";
-static const char azul[]            = "#6272a4";
-static const char fg[]		    = "#f8f8f2";
-static const unsigned int baralpha = 100;
-static const unsigned int borderalpha = 0;
+static const char azul[]            = "#2E3338";
+static const char fg[]		    = "#bfddb2";
+*/
+
+// gruvbox
+static const char col_gb_black[]     = "#131515";
+static const char col_gb_bg[]        = "#1d2021";
+static const char col_gb_bgm[]       = "#282828";
+static const char col_gb_bgs[]       = "#32302f";
+static const char col_gb_fg[]        = "#fbf1c7";
+
+static const char col_gb_red1[]     = "#cc241d";
+static const char col_gb_red2[]     = "#fb4934";
+
+static const char col_gb_green1[]   = "#98971a";
+static const char col_gb_green2[]   = "#b8bb26";
+
+static const char col_gb_yellow1[]  = "#d79921";
+static const char col_gb_yellow2[]  = "#fabd2f";
+
+static const char col_gb_blue1[]    = "#458588";
+static const char col_gb_blue2[]    = "#83a598";
+
+static const char col_gb_purple1[]  = "#b16286";
+static const char col_gb_purple2[]  = "#83869b";
+
+static const char col_gb_aqua1[]    = "#689d6a";
+static const char col_gb_aqua2[]    = "#8ec07c";
+
+static const char col_gb_gray1[]    = "#a89984";
+static const char col_gb_gray2[]    = "#928374";
+static const char col_gb_gray3[]    = "#665c54";
+static const char col_gb_gray4[]    = "#3c3836";
+static const char col_gb_gray5[]    = "#a89984";
+
+
+static const char col_gb_orange1[]  = "#d65d0e";
+static const char col_gb_orange2[]  = "#fe8019";
+
+
+static const unsigned int baralpha = 255;
+static const unsigned int borderalpha = 255;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { fg, cinza_e, azul },
-	[SchemeSel]  = { cinza_e, laranja_git,  cinza_c  },
-	[SchemeTitle] = { fg, cinza_e, cinza_e },
+	[SchemeNorm] = { col_gb_gray3, col_gb_bgs, col_gb_bg },
+	[SchemeSel]  = { col_gb_gray1, col_gb_bgs,  col_gb_gray4  },
+	[SchemeTitle] = { col_gb_gray3, col_gb_bgs, col_gb_bg },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -35,7 +73,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "[www]", "", "", "", "", "", "", "" };
+static const char *tags[] = { "[main]", "[www]", "[</>]", "[disc]", "[5]", "[6]", "[7]", "[8]", "[9]" };
 /*
 static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 */
@@ -74,8 +112,12 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", cinza_e , "-nf",fg , "-sb", laranja_git, "-sf", fg, NULL };
-static const char *termcmd[]  = { "konsole", NULL };
+/*static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gb_bgm , "-nf",col_gb_fg , "-sb", col_gb_orange2, "-sf", col_gb_bgs, NULL };
+*/
+
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL } ;
+  
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
